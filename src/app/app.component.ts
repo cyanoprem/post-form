cebda3d59e8ffa78dd55c0de90d31b1de6d18ff6
Cyano Prem <cyano.prem@gmail.com>
2022-12-16 09:13:47 +0530

    Complete Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'post-form';
+  value: string = '';
+  isChecked: boolean = false;
+
+  showText(value: string) {
+    this.value = value;
+  }
+
+  isItChecked() {
+    if(this.isChecked) {
+      
+    }
+  }
 }
